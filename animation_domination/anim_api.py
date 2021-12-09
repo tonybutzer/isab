2d634b119b88fa329922861523054830eb360325
EC2 Default User <ec2-user@ip-10-12-69-251.us-west-2.compute.internal>
2021-12-09 01:09:49 +0000

    tony auto dec2021

--- animation_domination/anim_api.py
+++ animation_domination/anim_api.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import os
 import sys
+import shutil
 
 sys.path.append('./mylib')
 
@@ -14,6 +16,25 @@ make_netcdf(aoi_filename_geojson, nc_filename)
 
 gif_filename = nc_filename.replace('.nc','.gif')
 make_animated_gif(nc_filename, gif_filename)
+
+
+def rm_if(filename):
+    if os.path.exists(filename):
+        os.remove(filename)
+
+
+ddir = './data'
+
+full_nc = f'{ddir}/{nc_filename}'
+full_gif = f'{ddir}/{gif_filename}'
+
+rm_if(full_nc)
+rm_if(full_gif)
+
+shutil.move(nc_filename, full_nc)
+shutil.move(gif_filename, full_gif)
+
+
 
 
 
